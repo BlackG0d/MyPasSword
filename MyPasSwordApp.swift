5c2a3f7d2f72840fd07f8f30d21339658ad90ddd
Nahapet Arshakyan <Arshakyan.nahapet@gmail.com>
2025-07-05 21:57:38 +0400

    Первый коммит

---
+++ MyPasSwordApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct MyPasSwordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.light)
+        }
+    }
+}
+ 
